c3b924fc546562e8ce7a090be3405b7392482279
bensonlu518 <do860518@gmail.com>
2023-04-25 16:37:30 +0800

    product - query products list

--- ecommerce-backend-api/src/main/java/com/bensonlu/ecommercebackendapi/dao/ProductDao.java
+++ ecommerce-backend-api/src/main/java/com/bensonlu/ecommercebackendapi/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.bensonlu.ecommercebackendapi.dao;
 import com.bensonlu.ecommercebackendapi.dto.ProductRequest;
 import com.bensonlu.ecommercebackendapi.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
--- ecommerce-backend-api/src/main/java/com/bensonlu/ecommercebackendapi/service/ProductService.java
+++ ecommerce-backend-api/src/main/java/com/bensonlu/ecommercebackendapi/service/ProductService.java
@@ -3,7 +3,11 @@ package com.bensonlu.ecommercebackendapi.service;
 import com.bensonlu.ecommercebackendapi.dto.ProductRequest;
 import com.bensonlu.ecommercebackendapi.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
