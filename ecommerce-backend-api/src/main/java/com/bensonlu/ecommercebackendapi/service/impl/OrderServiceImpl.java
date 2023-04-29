e10742ffbe902eebf2f46b7919e6c4b651ebe13c
bensonlu518 <do860518@gmail.com>
2023-04-29 19:07:24 +0800

    order - check user if exists and deduct stock

--- ecommerce-backend-api/src/main/java/com/bensonlu/ecommercebackendapi/service/impl/OrderServiceImpl.java
+++ ecommerce-backend-api/src/main/java/com/bensonlu/ecommercebackendapi/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.bensonlu.ecommercebackendapi.service.impl;
 
 import com.bensonlu.ecommercebackendapi.dao.OrderDao;
 import com.bensonlu.ecommercebackendapi.dao.ProductDao;
+import com.bensonlu.ecommercebackendapi.dao.UserDao;
 import com.bensonlu.ecommercebackendapi.dto.BuyItem;
 import com.bensonlu.ecommercebackendapi.dto.CreateOrderRequest;
 import com.bensonlu.ecommercebackendapi.model.Order;
 import com.bensonlu.ecommercebackendapi.model.OrderItem;
 import com.bensonlu.ecommercebackendapi.model.Product;
+import com.bensonlu.ecommercebackendapi.model.User;
 import com.bensonlu.ecommercebackendapi.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +24,17 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private final static Logger log= LoggerFactory.getLogger(OrderServiceImpl.class);
 
+    //insert mutilple Dao in service layer
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -39,6 +50,14 @@ public class OrderServiceImpl implements OrderService {
     @Transactional  //will rollback if throw exception
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        //check if user exists
+        User user =userDao.getUserById(userId);
+
+        if(user==null){
+            log.warn("userId: {} not exist", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         //caculate total amount
 
         int totalAmount=0;
@@ -46,6 +65,21 @@ public class OrderServiceImpl implements OrderService {
         for(BuyItem buyItem: createOrderRequest.getBuyItemList()){
             //get product information
             Product product=productDao.getProductById(buyItem.getProductId());
+
+            //check product if exist
+            if(product==null){
+                log.warn("product {} not exist",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            //check product if it is sufficient
+            }else if (product.getStock()< buyItem.getQuantity()){
+                log.warn("product {} is insufficient stock. Remaining stock: {}, the quantity you request: {}",
+                        buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                throw new ResponseStatusException((HttpStatus.BAD_REQUEST));
+            }
+
+            //if it exists then deduct the stock quantity
+            productDao.updateStock(product.getProductId(),product.getStock()- buyItem.getQuantity());
+
 
             //caculate amount
             int amount=buyItem.getQuantity()*product.getPrice();
@@ -63,7 +97,7 @@ public class OrderServiceImpl implements OrderService {
         //create order
         Integer orderId=orderDao.createOrder(userId,totalAmount);
 
-        //pass orderItemList to Dao
+        //create orderItems
         orderDao.createOrderItems(orderId,orderItemList);
 
         return orderId;
